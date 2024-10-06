d5d282f9b64d8ae21cdcfbc10d678759faa073a2
Lenovo <Lenovo@vishwajeet>
2024-10-06 22:30:05 +0530

    Initial Commit

---
+++ corejava/src/operators/Example3.java
@@ -0,0 +1,10 @@
+public class Rectangle
+{
+    public static void main(String[] args) 
+	{
+            int n = Integer.parseInt(args[0]);
+			int a = Integer.parseInt(args[1]);
+            int area = n*a;
+            System.out.println("area is " + area);
+    }
+}
