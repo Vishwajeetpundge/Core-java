337efd3b0f7008f3af8ff97428e0d5094d10edc1
Lenovo <Lenovo@172.16.0.161>
2024-10-08 12:17:38 +0530

    Commit

---
+++ corejava/src/Day_2/Pattern_Without_Loop.java
@@ -0,0 +1,28 @@
+package Day_2;
+
+public class Pattern_Without_Loop {
+
+
+	public static void main(String[]args) {
+		System.out.println("@@@@@@@@@@@");
+		System.out.println("@@       @@");
+		System.out.println("@@       @@");
+		System.out.println("@@       @@");
+		System.out.println("@@       @@");
+		System.out.println("@@       @@");
+		System.out.println("@@       @@");
+		System.out.println("@@@@@@@@@@@");
+	}
+
+}
+/*
+@@@@@@@@@@@
+@@       @@
+@@       @@
+@@       @@
+@@       @@
+@@       @@
+@@       @@
+@@@@@@@@@@@
+*/
+
