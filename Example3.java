284f6e7fd152a10de4206f0d0166fb8a785f954d
Vishwajeetpundge <pundgevishwajeet@gmail.com>
2024-11-19 23:45:11 +0530

    Add files via upload
    commit

---
+++ Example3.java
@@ -0,0 +1,11 @@
+package operators;
+public class Example3
+{
+    public static void main(String[] args) 
+	{
+            int n = Integer.parseInt(args[0]);
+			int a = Integer.parseInt(args[1]);
+            int area = n*a;
+            System.out.println("area is " + area);
+    }
+}
