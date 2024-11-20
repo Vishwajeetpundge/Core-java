dc0dc9f2267b4072c44addbacb0d0ceb14646b7f
Vishwajeetpundge <pundgevishwajeet@gmail.com>
2024-11-20 23:53:38 +0530

    Add files via upload
    commit

--- PlatformClassLoaderDemo.java
+++ PlatformClassLoaderDemo.java
@@ -3,14 +3,13 @@ class Foo
 {
 	
 }
-public class PlatformClassLoaderDemo {
 
-
-
+public class PlatformClassLoaderDemo 
+{
 	public static void main(String[] args) 
 	{
-		System.out.println("Super class of application class loader is :");
-		System.out.println(Foo.class.getClassLoader().getParent());
+		System.out.println("Super class of platform class loader is :");
+		System.out.println(Foo.class.getClassLoader().getParent().getParent());
 
 	}
 
