acdb3f2324e2691d3165a661347532724d89058f
Vishwajeetpundge <pundgevishwajeet@gmail.com>
2024-11-21 21:38:30 +0530

    Add files via upload
    commit

---
+++ StaticBlockDemo.java
@@ -0,0 +1,28 @@
+package Day_1;
+class Foo
+{		
+Foo()
+{
+	System.out.println("No Argument constructor..");
+}
+
+{
+	System.out.println("Instance block..");
+}
+
+static
+{
+	System.out.println("Static block...");
+}
+
+}
+
+public class StaticBlockDemo 
+	{	
+		public static void main(String [] args) 
+		{		
+			System.out.println("Main Method Executed ");		
+		}	
+		}
+
+
