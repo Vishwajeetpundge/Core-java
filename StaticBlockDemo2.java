7d438780efbd18effeb68e075e0f8adf30e29427
Vishwajeetpundge <pundgevishwajeet@gmail.com>
2024-11-21 21:56:08 +0530

    Add files via upload
    commit

---
+++ StaticBlockDemo2.java
@@ -0,0 +1,18 @@
+package Day_1;
+class Fooo
+{	
+static int x;
+
+static
+{		
+	System.out.println("x value is :"+x);
+}	
+}
+
+public class StaticBlockDemo2 
+	{
+		public static void main(String[] args) 
+		{
+			 new Fooo();
+		}
+		}
