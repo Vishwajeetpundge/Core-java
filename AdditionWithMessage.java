0ab6c838ddfc962658f8dddd2b8bf381cbd843b1
Vishwajeetpundge <pundgevishwajeet@gmail.com>
2024-11-23 23:41:41 +0530

    Add files via upload
    commit

---
+++ AdditionWithMessage.java
@@ -0,0 +1,12 @@
+package Day_0;
+
+public class AdditionWithMessage 
+{
+	public static void main(String[] args) 
+	{
+		int x = 10;
+		int y = 20;
+		int z = x + y;
+		System.out.println("Sum is :"+z);
+	}
+}
