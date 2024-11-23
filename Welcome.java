be693d13a7e411fa109f934e4468be5baeca5df3
Vishwajeetpundge <pundgevishwajeet@gmail.com>
2024-11-23 23:40:55 +0530

    Add files via upload
    commit

---
+++ Welcome.java
@@ -0,0 +1,15 @@
+package Day_0;
+
+public class Welcome   
+{
+	public static void main(String[] args) 
+	{	
+	  System.out.println("String array Variable");	 
+	}	
+
+	public static void main(int x) 
+	{	
+	  System.out.println("int Ordinary Variable ");	  
+	}	
+}
+
