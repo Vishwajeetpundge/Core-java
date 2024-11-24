58af4c7e1fcaa3c33cf6f89c24c723f06e7d68a4
Vishwajeetpundge <pundgevishwajeet@gmail.com>
2024-11-24 23:52:03 +0530

    Add files via upload
    commit

---
+++ AdditionWithout3rdVariable.java
@@ -0,0 +1,14 @@
+package Day_1;
+
+public class AdditionWithout3rdVariable 
+{
+	public static void main(String[] args) 
+	{
+		int x = 100;
+		int y = 200;		
+		System.out.println("Sum is :"+x+y); //100200
+		System.out.println(+x+y); //300
+		System.out.println(""+x+y);//100200
+		System.out.println("Sum is :"+(x+y)); //Sum is 300
+	}
+}
