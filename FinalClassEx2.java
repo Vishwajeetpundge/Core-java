a76918f1215ee216756bb041f3df8213d93ad117
Vishwajeetpundge <pundgevishwajeet@gmail.com>
2024-12-06 23:03:20 +0530

    Add files via upload
    commit

---
+++ FinalClassEx2.java
@@ -0,0 +1,20 @@
+package Day_0;
+
+final class Sample
+{
+	private Sample()
+	{
+	}
+	public void m1()
+	{
+		System.out.println("Sample class m1 method");
+	}
+}
+
+public class FinalClassEx2 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+}
