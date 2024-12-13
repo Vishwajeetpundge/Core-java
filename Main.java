fb70a6a87d296527a9e1be708003c7eee1d854c8
Vishwajeetpundge <pundgevishwajeet@gmail.com>
2024-12-14 00:00:13 +0530

    Add files via upload
    commit

--- Main.java
+++ Main.java
@@ -1,13 +1,109 @@
-package Day_23;
+package vishu_01;
+
+interface MediaPlayer 
+{
+	public abstract void play();
+	public abstract void stop();
+}
+interface AdvancedmediaPlayer extends MediaPlayer 
+{
+	public abstract void pause();
+
+}
+
+class MusicPlayer implements AdvancedmediaPlayer
+{
+	private String title;
+	
+	public MusicPlayer(String title)
+	{
+		if(title!=null && title!="")
+		{
+		this.title=title;
+		}
+		else
+		{
+			System.out.println("Title is not be empty string");
+		}
+	}
+	public void pause()
+	{
+		System.out.println("Media Pausing:"+title);
+	}
+	
+	public void play()
+	{
+		System.out.println("Media Started playing:"+title);
+	}
+	
+
+	public void stop()
+	{
+		System.out.println("Media Stopped:"+title);
+	}
+	
+	public String getTitle()
+	{
+		return this.title;
+	}
+}
+class VideoPlayer implements  AdvancedmediaPlayer
+{
+	private String title;
+	public VideoPlayer(String title)
+	{
+		if(title!=null && title!="")
+		{
+		this.title=title;
+		}
+		else
+		{
+			System.out.println("Title is not be empty string");
+		}
+	}
+		
+	public void play()
+	{
+		System.out.println("Video Playing Started");
+	}
+	
+	public void pause()
+	{
+		System.out.println("Video playing Stopped");
+	}
+	
+	public void stop()
+	{
+		System.out.println("Video Stopped");
+	}
+
+	public String getTitle()
+	{
+		return this.title;
+	}
+}
 
 
-public class Main {
-    public static void main(String[] args) {
-        // Test Case 1: Invalid input
-        Dog invalidDog = new Dog("", true, "Golden Retriever");
-        
-        // Test Case 2: Valid input
-        Dog myDog = new Dog("Buddy", true, "Golden Retriever");
-        myDog.displayDetails();
-    }
+public class Main 
+{
+
+	public static void main(String[] args)
+	{
+		MusicPlayer mp3=new MusicPlayer("Life");
+		System.out.println("Song Title:"+mp3.getTitle());
+		mp3.play();
+		mp3.stop();
+		mp3.pause();
+		
+		System.out.println("------------------------------------------------------");
+
+		VideoPlayer HD=new VideoPlayer("Video");
+		System.out.println("Song Title:"+HD.getTitle());
+		HD.play();
+		HD.pause();
+		HD.stop();
+		
+
+	}
+
 }
