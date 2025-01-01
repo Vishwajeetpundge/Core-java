e7f7fd644b582a69378bef0e8ce3123fa3484dc6
Vishwajeetpundge <pundgevishwajeet@gmail.com>
2025-01-01 14:18:05 +0530

    Add files via upload
    commit

---
+++ CarPunctureException.java
@@ -0,0 +1,10 @@
+package My_Package.O1.com;
+
+public class CarPunctureException extends Exception
+{
+	CarPunctureException(){}
+	CarPunctureException(String Puncture)
+	{
+		super(Puncture);
+	}
+}
