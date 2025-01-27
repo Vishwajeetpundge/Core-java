890a60dfd43507f7c700514315174a28d57a1447
Vishwajeetpundge <pundgevishwajeet@gmail.com>
2025-01-27 17:42:39 +0530

    Add files via upload
    commit

---
+++ NewPlatForm4.java
@@ -0,0 +1,36 @@
+package My_Package.O1.com;
+
+
+class NewPlatForm4
+{
+    public static void main(String args[])
+    {
+      {
+         new Thread()
+         {
+            public void run()
+              {
+            	for(int i=1;i<=(3^5);i++)
+            	{
+                System.out.println("Enjoy Your NewPlatForm..."+i);
+                break;
+            	}
+              }
+         }.start();
+
+        new Thread()
+           {
+              public void run()
+               {
+            	  for(int i=1;i<=(3^5);i++)
+              	{
+            		  int n=10;
+            		  if(i==(n/=n/=2))
+                  System.out.println("Experience this NewPlatForm..."+i);
+              	}
+               }
+           }.start();
+
+      }
+   } 
+}
