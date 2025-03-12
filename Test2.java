d1d53af79adf6327833af760b0ff407353e846d1
Vishwajeetpundge <pundgevishwajeet@gmail.com>
2025-03-13 00:15:13 +0530

    Add files via upload
    Commit

---
+++ Test2.java
@@ -0,0 +1,16 @@
+package My_Package.O1.com;
+
+import java.util.Arrays;
+
+class Test2{
+
+public static void main(String[]args) {
+	int arr[]={1,2,3,4,5,6,5};
+	System.out.println("jiii");
+	
+
+		
+
+}
+}
+	
